ce77896ed9a3dbc466cb649bd5a353f6605a576c
LuisP <luisjavierpolancomorel@gmail.com>
2021-05-22 00:48:55 -0400

    Updates

---
+++ precache-manifest.f52c3c1afdf1ce4f335640b707f7e79f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22064d2f51a9f3f90c15f403100b969",
+    "url": "/intro-component-with-signup-form/index.html"
+  },
+  {
+    "revision": "c04fe47c5444b0895199",
+    "url": "/intro-component-with-signup-form/static/css/main.bb21c6e1.chunk.css"
+  },
+  {
+    "revision": "30e1dec053556c1dcc5c",
+    "url": "/intro-component-with-signup-form/static/js/2.38feac30.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/intro-component-with-signup-form/static/js/2.38feac30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c04fe47c5444b0895199",
+    "url": "/intro-component-with-signup-form/static/js/main.b8771ac2.chunk.js"
+  },
+  {
+    "revision": "b006a6bd4bc6a1d7989e",
+    "url": "/intro-component-with-signup-form/static/js/runtime-main.af9856a0.js"
+  },
+  {
+    "revision": "eb8c9c1625fcb08ca7bfc046f015ea87",
+    "url": "/intro-component-with-signup-form/static/media/bg-intro-desktop.eb8c9c16.png"
+  },
+  {
+    "revision": "3e78c71a68ca0575e5ffb30e2e8ccc69",
+    "url": "/intro-component-with-signup-form/static/media/bg-intro-mobile.3e78c71a.png"
+  },
+  {
+    "revision": "eb2c6ce267c763a9a2682bfbb13739cc",
+    "url": "/intro-component-with-signup-form/static/media/icon-error.eb2c6ce2.svg"
+  }
+]);
